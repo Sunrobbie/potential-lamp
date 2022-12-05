d7951a000b08bf13ac7805822fc7eea7cc256411
Robert Sunbury <sunrobbie00@gmail.com>
2022-12-05 13:09:47 -0500

    Query update

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ con = sqlite3.connect("BaseStation.sqb");
 
 cur = con.cursor()
 
-res =  cur.execute("SELECT * FROM Flights");
+res =  cur.execute("SELECT Callsign, Firstlat, Lastlat, Firstlon, Lastlon FROM Flights WHERE Callsign IS NOT '';");
 
 #res = cur.execute("SELECT table_name FROM all_tables ORDER BY table_name ASC;")
+
+print(res.fetchall())
+
+res = cur.execute("SELECT Registration FROM Aircraft WHERE Registration LIKE Raytheon")
 
 print(res.fetchall())
 
