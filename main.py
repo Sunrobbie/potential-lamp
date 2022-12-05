530ac0aafbed9105c51d61900733526f63d503e7
Robert Sunbury <sunrobbie00@gmail.com>
2022-12-05 16:58:39 -0500

    Added more stuff

--- main.py
+++ main.py
@@ -5,11 +5,33 @@ con = sqlite3.connect("BaseStation.sqb");
 
 cur = con.cursor()
 
-res =  cur.execute("SELECT Callsign, Firstlat, Lastlat, Firstlon, Lastlon FROM Flights WHERE Callsign IS NOT ''");
+#res =  cur.execute("SELECT Callsign, Firstlat, Lastlat, Firstlon, Lastlon FROM Flights WHERE Callsign IS NOT ''");
 
 #res = cur.execute("SELECT table_name FROM all_tables ORDER BY table_name ASC;")
 
-print(res.fetchall())
+#print(res.fetchall())
+
+res =  cur.execute("SELECT Callsign, Firstlat, Lastlat, Firstlon, Lastlon, FirstGroundSpeed, LastGroundSpeed FROM Flights");
+
+allFlights = res.fetchall()
+
+flightSpeeds = list(filter(lambda item: item[5] is not None and item[6] is not None, allFlights))
+
+flightSpeeds = list(map(lambda item: item[5], flightSpeeds)) + list(map(lambda item: item[6], flightSpeeds))
+
+plt.figure()
+
+for flight in allFlights:
+    x = (flight[1], flight[2])
+    y = (flight[3], flight[4])
+    plt.plot(x, y)
+
+plt.figure()
+
+
+plt.hist(flightSpeeds)
+
+plt.figure()
 
 res = cur.execute("SELECT AircraftID FROM Aircraft WHERE Manufacturer LIKE 'Raytheon%'")
 
@@ -24,9 +46,10 @@ res =  cur.execute(queryM % "'59', '561', '599'")
 militData = res.fetchall()
 
 for flight in militData:
-    x = (flight[1], flight[1][2])
-    y = (flight[3], flight[1][4])
+    x = (flight[1], flight[2])
+    y = (flight[3], flight[4])
+    plt.plot(x, y)
 
-plt.plot(x, y)
+
 
 con.close()
